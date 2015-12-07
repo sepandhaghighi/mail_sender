6f88bfda7f3ce36e119a79159588b506c65e0f2e
= <sepand.haghighi@yahoo.com>
2015-12-08 01:46:31 +0330

    feat : basic program added

---
+++ aut_smtp/aut_smtp/Form1.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Net.Mail
+Public Class Form1
+    Dim smtpserver As New SmtpClient
+    Dim mail As New MailMessage
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        End
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        smtpserver.Host = "smtp.gmail.com"
+        smtpserver.Port = 587
+        smtpserver.EnableSsl = True
+
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        mail.To.Add(TextBox5.Text)
+        mail.From = New MailAddress("questionnaire1394@gmail.com", "Questionnare", System.Text.Encoding.UTF8)
+        mail.IsBodyHtml = True
+        mail.Subject = TextBox4.Text
+        mail.Body = TextBox3.Text
+        smtpserver.Send(mail)
+        MsgBox("Message Sent")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        smtpserver.Credentials = New System.Net.NetworkCredential("questionnaire1394@gmail.com", "Manchesterderbyqwe1")
+
+    End Sub
+End Class
---
+++ aut_smtp/aut_smtp/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("aut_smtp")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("aut_smtp")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b42dcbf3-c667-46cb-9b1f-05701cc2e243")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
