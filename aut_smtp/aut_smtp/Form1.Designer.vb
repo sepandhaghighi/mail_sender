b637d2aac5805e4860a7e362ab8d1620893424a7
= <sepand.haghighi@yahoo.com>
2015-12-08 22:31:37 +0330

    feat : yahoo google and live server added

--- aut_smtp/aut_smtp/Form1.Designer.vb
+++ aut_smtp/aut_smtp/Form1.Designer.vb
@@ -30,16 +30,23 @@ Partial Class Form1
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.Button2 = New System.Windows.Forms.Button()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Button3 = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(339, 326)
+        Me.Button1.Location = New System.Drawing.Point(754, 340)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(129, 50)
         Me.Button1.TabIndex = 0
@@ -51,21 +58,21 @@ Partial Class Form1
         Me.TextBox3.Location = New System.Drawing.Point(12, 165)
         Me.TextBox3.Multiline = True
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(305, 211)
+        Me.TextBox3.Size = New System.Drawing.Size(487, 207)
         Me.TextBox3.TabIndex = 5
         '
         'TextBox4
         '
         Me.TextBox4.Location = New System.Drawing.Point(12, 127)
         Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(305, 20)
+        Me.TextBox4.Size = New System.Drawing.Size(671, 20)
         Me.TextBox4.TabIndex = 6
         '
         'TextBox5
         '
         Me.TextBox5.Location = New System.Drawing.Point(12, 101)
         Me.TextBox5.Name = "TextBox5"
-        Me.TextBox5.Size = New System.Drawing.Size(305, 20)
+        Me.TextBox5.Size = New System.Drawing.Size(671, 20)
         Me.TextBox5.TabIndex = 7
         '
         'TextBox1
@@ -79,7 +86,6 @@ Partial Class Form1
         '
         Me.TextBox2.Location = New System.Drawing.Point(113, 47)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
         Me.TextBox2.Size = New System.Drawing.Size(148, 20)
         Me.TextBox2.TabIndex = 2
         Me.TextBox2.UseSystemPasswordChar = True
@@ -102,19 +108,10 @@ Partial Class Form1
         Me.Label2.TabIndex = 4
         Me.Label2.Text = "Password"
         '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(318, 23)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(129, 43)
-        Me.Button2.TabIndex = 8
-        Me.Button2.Text = "Login"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(336, 108)
+        Me.Label3.Location = New System.Drawing.Point(689, 104)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(22, 13)
         Me.Label3.TabIndex = 9
@@ -123,7 +120,7 @@ Partial Class Form1
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(336, 134)
+        Me.Label4.Location = New System.Drawing.Point(689, 130)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(43, 13)
         Me.Label4.TabIndex = 10
@@ -132,7 +129,7 @@ Partial Class Form1
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(336, 168)
+        Me.Label5.Location = New System.Drawing.Point(505, 168)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(50, 13)
         Me.Label5.TabIndex = 11
@@ -140,23 +137,96 @@ Partial Class Form1
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(342, 259)
+        Me.Button3.Location = New System.Drawing.Point(754, 269)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(125, 48)
         Me.Button3.TabIndex = 12
         Me.Button3.Text = "Send"
         Me.Button3.UseVisualStyleBackColor = True
         '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(17, 16)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(56, 17)
+        Me.RadioButton1.TabIndex = 13
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Yahoo"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(17, 39)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(59, 17)
+        Me.RadioButton2.TabIndex = 14
+        Me.RadioButton2.Text = "Google"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(17, 64)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(45, 17)
+        Me.RadioButton3.TabIndex = 15
+        Me.RadioButton3.Text = "Live"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton3)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Location = New System.Drawing.Point(779, 21)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(100, 87)
+        Me.GroupBox1.TabIndex = 16
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Provider"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(276, 47)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(68, 26)
+        Me.Button2.TabIndex = 17
+        Me.Button2.Text = "Show"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(563, 166)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 212)
+        Me.ListBox1.TabIndex = 18
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(711, 101)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(46, 19)
+        Me.Button5.TabIndex = 20
+        Me.Button5.Text = "Add"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(480, 388)
+        Me.ClientSize = New System.Drawing.Size(895, 402)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.TextBox5)
         Me.Controls.Add(Me.TextBox4)
         Me.Controls.Add(Me.TextBox3)
@@ -167,6 +237,8 @@ Partial Class Form1
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "MailSender"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -179,10 +251,16 @@ Partial Class Form1
     Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents Button2 As System.Windows.Forms.Button
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents Label4 As System.Windows.Forms.Label
     Friend WithEvents Label5 As System.Windows.Forms.Label
     Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button5 As System.Windows.Forms.Button
 
 End Class
--- aut_smtp/aut_smtp/Form1.vb
+++ aut_smtp/aut_smtp/Form1.vb
@@ -2,15 +2,43 @@
 Public Class Form1
     Dim smtpserver As New SmtpClient
     Dim mail As New MailMessage
+    Public Shared pass_flag As Boolean = True
+    Public Sub host_port_set(ByVal index As Integer)
+        If index = 1 Then
+            smtpserver.Host = "smtp.mail.yahoo.com"
+            smtpserver.Host = 465
+            smtpserver.EnableSsl = False
+        ElseIf index = 2 Then
+            smtpserver.Host = "smtp.gmail.com"
+            smtpserver.Port = 587
+        ElseIf index = 3 Then
+            smtpserver.Host = "smtp.live.com"
+            smtpserver.Port = 587
+        End If
+    End Sub
+    Public Function empty_message() As Boolean
+        Dim result As Boolean
+        If TextBox1.Text.Contains("@") = False Or Len(TextBox1.Text) = 0 Then
+            MsgBox("Please Enter Your Complete Mail With @")
+        ElseIf Len(TextBox2.Text) = 0 Then
+            MsgBox("Please Enter Your Password")
+        ElseIf ListBox1.Items.Count = 0 Then
+            MsgBox("Please Enter Reciever Address Complete With @")
+        ElseIf Len(TextBox5.Text) = 0 Then
+            MsgBox("Please Enter Subject Of Your Mail")
+        Else
+            result = True
+        End If
+        Return result
+    End Function
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         End
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        smtpserver.Host = "smtp.gmail.com"
-        smtpserver.Port = 587
         smtpserver.EnableSsl = True
 
+        host_port_set(1)
 
     End Sub
 
@@ -19,17 +47,71 @@ Public Class Form1
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        mail.To.Add(TextBox5.Text)
-        mail.From = New MailAddress("questionnaire1394@gmail.com", "Questionnare", System.Text.Encoding.UTF8)
-        mail.IsBodyHtml = True
-        mail.Subject = TextBox4.Text
-        mail.Body = TextBox3.Text
-        smtpserver.Send(mail)
-        MsgBox("Message Sent")
+        On Error GoTo error_label
+        Dim flag As Boolean
+        flag = empty_message()
+        If flag = True Then
+            smtpserver.Credentials = New System.Net.NetworkCredential(TextBox1.Text, TextBox2.Text)
+            TextBox1.Text = (ListBox1.Items.Count)
+            'mail.To.Add(ListBox1.Items.ToString)
+            'mail.From = New MailAddress(TextBox1.Text, TextBox1.Text, System.Text.Encoding.UTF8)
+            'mail.IsBodyHtml = True
+            'mail.Subject = TextBox4.Text
+            'mail.Body = TextBox3.Text
+            'smtpserver.Send(mail)
+            'MsgBox("Message Sent")
+        End If
+
+        Exit Sub
+error_label:
+        MsgBox("Error In Send")
+
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        smtpserver.Credentials = New System.Net.NetworkCredential("questionnaire1394@gmail.com", "Manchesterderbyqwe1")
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
 
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        host_port_set(2)
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        host_port_set(1)
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        host_port_set(3)
+    End Sub
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        If pass_flag = True Then
+            TextBox2.UseSystemPasswordChar = False
+            pass_flag = False
+            Button2.Text = "Hide"
+        Else
+            TextBox2.UseSystemPasswordChar = True
+            pass_flag = True
+            Button2.Text = "Show"
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If ListBox1.Items.Contains(TextBox5.Text) Then
+            MsgBox("This Email Is Present In Mail List")
+        ElseIf TextBox5.Text.Contains("@") And TextBox5.Text.Contains(".") = True Then
+            ListBox1.Items.Add(TextBox5.Text)
+            TextBox5.Text = ""
+        Else
+            MsgBox("Please Enter Correct Email Address")
+        End If
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        ListBox1.Items.Remove(ListBox1.SelectedItem)
     End Sub
 End Class
